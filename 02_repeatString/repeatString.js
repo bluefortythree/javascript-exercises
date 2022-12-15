6befda909e9e1e6cafa11a7209ba512bd2129e3e
bluefortythree <117217594+bluefortythree@users.noreply.github.com>
2022-12-15 12:46:28 -0800

    complete exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,25 @@
-const repeatString = function() {
+const repeatString = function(string, number) {
+        let newString = '';
+        if (number > 0) {
+            while (number > 0) {
+            newString += string;
+            number--;
+        } 
+    } else if (number === 0 || string === '') {
+        newString;
+    } else {
+        newString = 'ERROR'
+    }
+        return newString;   
+    }
 
-};
+repeatString('hey',3);
+repeatString('hey',10);
+repeatString('hey',1);
+repeatString('hey',0);
+repeatString('hey',-1);
+repeatString('hey',Math.floor(Math.random() * 1000));
+repeatString('',10);
 
 // Do not edit below this line
 module.exports = repeatString;
