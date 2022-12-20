c39c0b3dbcf3e3a2a2a6415e1399a01dd9c164fb
bluefortythree <117217594+bluefortythree@users.noreply.github.com>
2022-12-19 16:20:48 -0800

    complete exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degrees) {
+  let celcius = (degrees - 32) * 5/9;
+  if(Number.isInteger(celcius)) {
+    return celcius
+  } else {
+    return +celcius.toFixed(1) // the + makes it a number, why does it return as a string in the first place? 
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degrees) {
+  let fahrenheit = (degrees * 9/5) + 32;
+  if(Number.isInteger(fahrenheit)) {
+    return fahrenheit
+  } else {
+    return +fahrenheit.toFixed(1)
+  }
 };
 
 // Do not edit below this line
